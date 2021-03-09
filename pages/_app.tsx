9b9d046ef42f7f52064c84024be0981e1f74f56d
Maciej aXenDev Balcerzak <ithereplay@gmail.com>
2021-03-09 11:02:43 +0100

    Add Poppins font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
-import '../styles/globals.scss';
+import '../styles/global.scss';
 
-function MyApp({ Component, pageProps }) {
+const MyApp = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
-}
+};
 
 export default MyApp;
