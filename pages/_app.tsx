3ca2280ba58a9ed1b9cc5448836a87bfb487b6df
Maciej aXenDev Balcerzak <ithereplay@gmail.com>
2021-03-10 17:50:46 +0100

    Add progressbar when page is loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import '../styles/global.scss';
+import NextNprogress from 'nextjs-progressbar';
 
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NextNprogress color="var(--color-main)" startPosition={0.3} stopDelayMs={200} height={3} />
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
