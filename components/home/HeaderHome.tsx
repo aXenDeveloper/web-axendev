6f689a8b1ebc88a06933d4e8f26129a2b4bd11e6
Maciej aXenDev Balcerzak <ithereplay@gmail.com>
2021-03-14 19:59:46 +0100

    Change img background to video in home page

--- components/home/HeaderHome.tsx
+++ components/home/HeaderHome.tsx
@@ -5,12 +5,13 @@ const HeaderHome = () => {
 
   return (
     <section className="homeTop">
-      <div className="container">
-        <h1 className="homeTop_text">
-          {t('home_header_text_amazing')}{' '}
-          <span className="text-weight:bold text:color">{t('home_header_text_projects')}</span> {t('home_header_text_for')}{' '}
-          <span className="text-weight:bold text:color">{t('home_header_text_you')}</span>!
-        </h1>
+      <video autoPlay muted loop className="homeTop_video">
+        <source src="/assets/header_home.mp4" />
+      </video>
+
+      <div className="homeTop_text">
+        {t('home_header_text_amazing')} <span className="text:color">{t('home_header_text_projects')}</span>{' '}
+        {t('home_header_text_for')} <span className="text:color">{t('home_header_text_you')}</span>!
       </div>
     </section>
   );
