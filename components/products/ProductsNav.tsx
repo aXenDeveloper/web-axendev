a0711ca4c04a060ace22ce584cbb8a3649c9eb1d
Maciej aXenDev Balcerzak <ithereplay@gmail.com>
2021-03-19 16:05:16 +0100

    Add select input

--- components/products/ProductsNav.tsx
+++ components/products/ProductsNav.tsx
@@ -1,19 +1,22 @@
+import useTranslation from 'next-translate/useTranslation';
 import { FC } from 'react';
 import { ProductsCategoryType } from '../../types/components/productsTypes';
 import ProductsCategory from './ProductsCategory';
 
 const ProductsNav: FC<ProductsCategoryType> = ({ handleSelectSortData, sortData }) => {
+  const { t } = useTranslation('global');
+
   return (
     <ul className="products_nav">
       <ProductsCategory />
 
       <li className="box">
         <div className="box_title">
-          <h3>Filter</h3>
+          <h3>{t('products_filter')}</h3>
         </div>
 
         <div className="padding:half">
-          <select onChange={el => handleSelectSortData(el.target.value)} value={sortData}>
+          <select onChange={el => handleSelectSortData(el.target.value)} value={sortData} className="input input_select">
             <option value="DESC">DESC</option>
             <option value="ASC">ASC</option>
             <option value="priceDESC">priceDESC</option>
