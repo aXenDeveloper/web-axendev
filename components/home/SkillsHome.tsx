0623c71a48590107725a66d0a73aec58a0e4e045
Maciej aXenDev Balcerzak <ithereplay@gmail.com>
2021-03-21 11:25:02 +0100

    Add title in skills section, add highlight items in skills list

--- components/home/SkillsHome.tsx
+++ components/home/SkillsHome.tsx
@@ -1,31 +1,80 @@
-import { faCogs, faPaintBrush, faTools } from '@fortawesome/free-solid-svg-icons';
+import { faCss3, faGithub, faHtml5, faJs, faNodeJs, faPhp, faReact } from '@fortawesome/free-brands-svg-icons';
+import { faCode, faCogs, faPaintBrush, faTools } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import useTranslation from 'next-translate/useTranslation';
 import SkillsHomeItem from './SkillsHomeItem';
 
 const SkillsHome = () => {
+  const { t } = useTranslation('global');
+
   return (
     <section className="home_skills">
       <div className="container">
         <header className="home_skills_title">
-          <h2>Skills</h2>
-          <p>Test something desc section</p>
+          <h2>{t('home_skills_title')}</h2>
+          <p>{t('home_skills_desc')}</p>
         </header>
 
         <ul className="home_skills_contain">
           <SkillsHomeItem title="Back-end" icon={faCogs}>
+            <li className="home_skills_contain_list_highlight">
+              <FontAwesomeIcon icon={faPhp} />
+              <span>PHP</span>
+            </li>
+
+            <li className="home_skills_contain_list_highlight">
+              <FontAwesomeIcon icon={faNodeJs} />
+              <span>NodeJS</span>
+            </li>
+
+            <li>ExpressJS</li>
+            <li>MongoDB</li>
+            <li>MySQL / MariaDB</li>
             <li>C#</li>
           </SkillsHomeItem>
 
           <SkillsHomeItem title="Front-end" icon={faPaintBrush} highlight>
-            <li>HTML</li>
-            <li>CSS / SASS</li>
-            <li>JavaScript</li>
-            <li>HTML</li>
-            <li>CSS / SASS</li>
-            <li>JavaScript</li>
+            <li className="home_skills_contain_list_highlight">
+              <FontAwesomeIcon icon={faJs} />
+              <span>JavaScript</span>
+            </li>
+
+            <li className="home_skills_contain_list_highlight">
+              <FontAwesomeIcon icon={faReact} />
+              <span>React (NextJS)</span>
+            </li>
+
+            <li className="home_skills_contain_list_highlight">
+              <FontAwesomeIcon icon={faHtml5} />
+              <span>HTML</span>
+            </li>
+
+            <li className="home_skills_contain_list_highlight">
+              <FontAwesomeIcon icon={faCss3} />
+              <span>CSS / SASS</span>
+            </li>
+
+            <li>TypeScript</li>
+            <li>Jest</li>
+            <li>SEO</li>
+            <li>UX / UI</li>
           </SkillsHomeItem>
 
           <SkillsHomeItem title="Tools" icon={faTools}>
-            <li>VS Code</li>
+            <li className="home_skills_contain_list_highlight">
+              <FontAwesomeIcon icon={faGithub} />
+              <span>Git</span>
+            </li>
+
+            <li className="home_skills_contain_list_highlight">
+              <FontAwesomeIcon icon={faCode} />
+              <span>VS Code</span>
+            </li>
+
+            <li>IPS Community Suite</li>
+            <li>Adobe Photoshop</li>
+            <li>Adobe Illustrator</li>
+            <li>Webpack</li>
           </SkillsHomeItem>
         </ul>
       </div>
