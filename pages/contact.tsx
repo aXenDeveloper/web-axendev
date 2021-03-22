03d873cd6d42dc1b2c4b4a6eed5de6248156ad12
Maciej aXenDev Balcerzak <ithereplay@gmail.com>
2021-03-22 22:34:26 +0100

    Add icon in contact list

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -3,6 +3,9 @@ import Layout from '../components/layouts/Layout';
 import useTranslation from 'next-translate/useTranslation';
 import ContactForm from '../components/contact/ContactForm';
 import Breadcrumb from '../components/layouts/Breadcrumb';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faComments, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faProductHunt } from '@fortawesome/free-brands-svg-icons';
 
 const ContactPage = () => {
   const { t } = useTranslation('global');
@@ -18,38 +21,43 @@ const ContactPage = () => {
       <main className="container container:small">
         <h1 className="page_title">{t('page_contact')}</h1>
 
-        <div className="contact">
+        <div className="contact box padding">
           <ContactForm />
 
-          <ul className="contact_other">
-            <li className="box">
-              <div className="box_title">
-                <h3>Contact list</h3>
-              </div>
-              <div className="padding:half">
-                <ul className="contact_other_list">
-                  <li>
-                    <a href="mailto:ithereplay@gmail.com">ithereplay@gmail.com</a>
-                  </li>
-                  <li>
-                    <a href="https://invisioncommunity.com/profile/580858-axendev/" target="blank" rel="noopener nofollow">
-                      Invisioncommunity.com
-                    </a>
-                  </li>
-                  <li>
-                    <a href="https://invisioncommunity.com/profile/580858-axendev/" target="blank" rel="noopener nofollow">
-                      Invisionize.pl
-                    </a>
-                  </li>
-                  <li>
-                    <a href="https://pecetowicz.pl/profile/36319-axendev/" target="blank" rel="noopener nofollow">
-                      Pecetowicz.pl
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </li>
-          </ul>
+          <div className="contact_other padding:half">
+            <ul className="contact_other_list">
+              <li className="contact_other_list_item">
+                <span className="contact_other_list_item_icon">
+                  <FontAwesomeIcon icon={faEnvelope} />
+                </span>
+                <a href="mailto:ithereplay@gmail.com">ithereplay@gmail.com</a>
+              </li>
+              <li className="contact_other_list_item">
+                <span className="contact_other_list_item_icon">
+                  <FontAwesomeIcon icon={faComments} />
+                </span>
+                <a href="https://invisioncommunity.com/profile/580858-axendev/" target="blank" rel="noopener nofollow">
+                  Invisioncommunity.com
+                </a>
+              </li>
+              <li className="contact_other_list_item">
+                <span className="contact_other_list_item_icon">
+                  <FontAwesomeIcon icon={faComments} />
+                </span>
+                <a href="https://invisioncommunity.com/profile/580858-axendev/" target="blank" rel="noopener nofollow">
+                  forum.Invisionize.pl
+                </a>
+              </li>
+              <li className="contact_other_list_item">
+                <span className="contact_other_list_item_icon">
+                  <FontAwesomeIcon icon={faProductHunt} />
+                </span>
+                <a href="https://pecetowicz.pl/profile/36319-axendev/" target="blank" rel="noopener nofollow">
+                  Pecetowicz.pl
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </main>
 
