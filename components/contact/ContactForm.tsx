a9ce893d88bd3337fa4d0ee54c272891d0319cda
Maciej aXenDev Balcerzak <ithereplay@gmail.com>
2021-03-22 11:25:50 +0100

    Add clear form, styled form in contact page, change style button light

--- components/contact/ContactForm.tsx
+++ components/contact/ContactForm.tsx
@@ -1,49 +1,113 @@
-import { FormEvent } from 'react';
+import useTranslation from 'next-translate/useTranslation';
+import { ChangeEvent, FormEvent, useState } from 'react';
 
 const ContactForm = () => {
+  const [inputName, setInputName] = useState('');
+  const [inputEmail, setInputEmail] = useState('');
+  const [inputTitle, setInputTitle] = useState('');
+  const [inputText, setInputText] = useState('');
+
+  const handleInputName = (e: ChangeEvent<HTMLInputElement>) => setInputName(e.target.value);
+  const handleInputEmail = (e: ChangeEvent<HTMLInputElement>) => setInputEmail(e.target.value);
+  const handleInputTitle = (e: ChangeEvent<HTMLInputElement>) => setInputTitle(e.target.value);
+  const handleInputText = (e: ChangeEvent<HTMLTextAreaElement>) => setInputText(e.target.value);
+
+  const { t } = useTranslation('global');
+
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
   };
 
+  const handleClearButton = () => {
+    setInputName('');
+    setInputEmail('');
+    setInputTitle('');
+    setInputText('');
+  };
+
   return (
     <div className="contact_content">
-      <h2>Contact by form</h2>
+      <h2>{t('contact_form')}</h2>
 
       <div className="box padding">
         <form action="https://files.axendev.net/mail.php" method="POST" onSubmit={handleSubmit}>
           <ul className="form">
             <li className="form_input form_input_text">
-              <input type="text" id="name" name="name" className="input input_text" placeholder=" " required />
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="input input_text"
+                placeholder=" "
+                onChange={handleInputName}
+                value={inputName}
+                required
+              />
               <label htmlFor="name" className="input_label">
-                Wpisz tutaj swoją nazwę
+                {t('contact_form_name')}
               </label>
             </li>
 
             <li className="form_input form_input_text">
-              <input type="email" id="email" name="email" className="input input_text" placeholder=" " required />
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="input input_text"
+                placeholder=" "
+                onChange={handleInputEmail}
+                value={inputEmail}
+                required
+              />
               <label htmlFor="email" className="input_label">
-                Email
+                {t('contact_form_email')}
               </label>
             </li>
 
             <li className="form_input form_input_text">
-              <input type="text" id="title" name="title" className="input input_text" placeholder=" " required />
+              <input
+                type="text"
+                id="title"
+                name="title"
+                className="input input_text"
+                placeholder=" "
+                onChange={handleInputTitle}
+                value={inputTitle}
+                required
+              />
               <label htmlFor="title" className="input_label">
-                Title
+                {t('contact_form_title')}
               </label>
             </li>
 
             <li className="form_input form_input_textarea">
-              <textarea id="text" name="text" className="input input_textarea" required></textarea>
+              <textarea
+                id="text"
+                name="text"
+                className="input input_textarea"
+                onChange={handleInputText}
+                value={inputText}
+                required
+              ></textarea>
               <label htmlFor="text" className="input_label">
-                Treść
+                {t('contact_form_text')}
               </label>
             </li>
           </ul>
 
-          <button type="submit" className="button button_medium button_primary">
-            <span>Send</span>
-          </button>
+          <ul className="form_bottom">
+            <li>
+              <button type="submit" className="button button_medium button_primary">
+                <span>{t('contact_form_send')}</span>
+              </button>
+            </li>
+
+            <li>
+              <button type="button" className="button button_medium button_light" onClick={handleClearButton}>
+                <span>{t('contact_form_clear')}</span>
+              </button>
+            </li>
+          </ul>
         </form>
       </div>
     </div>
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -12,13 +12,41 @@ const ContactPage = () => {
         <title>{t('page_contact')} - aXenDev.net</title>
       </Head>
 
-      <main className="container">
+      <main className="container container:small">
         <h1 className="page_title">{t('page_contact')}</h1>
 
         <div className="contact">
           <ContactForm />
 
-          <div className="contact_other">test123</div>
+          <ul className="contact_other">
+            <li className="box">
+              <div className="box_title">
+                <h3>Contact list</h3>
+              </div>
+              <div className="padding:half">
+                <ul className="contact_other_list">
+                  <li>
+                    <a href="mailto:ithereplay@gmail.com">ithereplay@gmail.com</a>
+                  </li>
+                  <li>
+                    <a href="https://invisioncommunity.com/profile/580858-axendev/" target="blank" rel="noopener nofollow">
+                      Invisioncommunity.com
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://invisioncommunity.com/profile/580858-axendev/" target="blank" rel="noopener nofollow">
+                      Invisionize.pl
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://pecetowicz.pl/profile/36319-axendev/" target="blank" rel="noopener nofollow">
+                      Pecetowicz.pl
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+          </ul>
         </div>
       </main>
     </Layout>
