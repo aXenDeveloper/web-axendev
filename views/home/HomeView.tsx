b477327f19fd4130511e80ca82ea689c68ebda29
aXenDeveloper <axendeveloper@gmail.com>
2023-07-07 14:10:42 +0200

    Add buttons, update packages

--- views/home/HomeView.tsx
+++ views/home/HomeView.tsx
@@ -2,6 +2,7 @@ import cx from 'classnames';
 import { useTranslations } from 'next-intl';
 
 import style from './HomeView.module.scss';
+import { Button } from '@/components/button/Button';
 
 export const HomeView = () => {
   const t = useTranslations('home');
@@ -10,6 +11,13 @@ export const HomeView = () => {
     <div className={cx('layout_wrapper', style.wrapper)}>
       <h1 className={style.title}>{t('title')}</h1>
       <p className={style.desc}>{t('desc')}</p>
+
+      <div className={style.buttons}>
+        <Button id="test">Test</Button>
+        <Button kind="secondary" id="test">
+          Test
+        </Button>
+      </div>
     </div>
   );
 };
