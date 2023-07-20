227573b35a8030ab1558c8d339920faa39d583f6
aXenDeveloper <axendeveloper@gmail.com>
2023-07-20 21:00:11 +0200

    Add swiperjs thumbs for project view

---
+++ components/lightbox/LightBox.tsx
@@ -0,0 +1,3 @@
+export const LightBox = () => {
+  return <div>LightBox</div>;
+};
--- views/products/[id]/swiper/SwiperProduct.tsx
+++ views/products/[id]/swiper/SwiperProduct.tsx
@@ -1,28 +1,66 @@
 'use client';
 
-import { Navigation } from 'swiper/modules';
+import { Navigation, Thumbs, FreeMode } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { useState } from 'react';
+import ThumbsSwiper from 'swiper';
 
 import { Img } from '@/components/img/Img';
 import style from './SwiperProduct.module.scss';
+
 import 'swiper/css';
 import 'swiper/css/navigation';
+import 'swiper/css/thumbs';
+import 'swiper/css/free-mode';
 
 interface Props {
   images: string[];
 }
 
 export const SwiperProduct = ({ images }: Props) => {
+  const [thumbsSwiper, setThumbsSwiper] = useState<ThumbsSwiper | null>(null);
+
   return (
-    <Swiper
-      slidesPerView="auto"
-      className={style.wrapper}
-      modules={[Navigation]}
-      navigation
-    >
-      {images.map(image => (
-        <SwiperSlide key={image}>
-          <button type="button" className={style.item}>
+    <>
+      <Swiper
+        slidesPerView={1}
+        className={style.swiper}
+        modules={[Navigation, Thumbs, FreeMode]}
+        thumbs={{ swiper: thumbsSwiper }}
+        navigation
+      >
+        {images.map(image => (
+          <SwiperSlide key={image}>
+            <button type="button" className={style.item}>
+              <Img
+                src={image}
+                alt=""
+                fill
+                sizes="
+            (max-width: 768px) 100vw,
+            (max-width: 1200px) 50vw,
+            33vw"
+              />
+            </button>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <Swiper
+        onSwiper={setThumbsSwiper}
+        spaceBetween={10}
+        slidesPerView={2}
+        freeMode
+        watchSlidesProgress
+        modules={[Navigation, Thumbs, FreeMode]}
+        className={style.thumbs}
+        breakpoints={{
+          500: {
+            slidesPerView: 4
+          }
+        }}
+      >
+        {images.map(image => (
+          <SwiperSlide className={style.thumbs_item} key={image}>
             <Img
               src={image}
               alt=""
@@ -31,11 +69,10 @@ export const SwiperProduct = ({ images }: Props) => {
             (max-width: 768px) 100vw,
             (max-width: 1200px) 50vw,
             33vw"
-              priority
             />
-          </button>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
   );
 };
