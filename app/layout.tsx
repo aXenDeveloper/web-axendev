b2821057cad0b3d1f9e76c01f4a65a35787f0887
aXenDeveloper <axendeveloper@gmail.com>
2023-08-02 23:04:10 +0200

    Add og

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ import { Metadata } from 'next';
 import { CONFIG_TITLE } from '../config';
 
 export function generateMetadata(): Metadata {
+  const desc =
+    "I'm Maciej Piotr Balcerzak and I'm working as Software Engineer with a passion for creating beautiful and user-friendly products. I have experience in front-end, back-end, testing and UX/UI design, but my primary experience is front-end. Also I'm contributor for Invision Community making themes and apps.";
+
   return {
     title: {
       default: CONFIG_TITLE,
@@ -13,9 +16,20 @@ export function generateMetadata(): Metadata {
     icons: {
       shortcut: '/icons/favicon.ico'
     },
-    description:
-      "I'm Maciej Piotr Balcerzak and I'm working as Software Engineer with a passion for creating beautiful and user-friendly products. I have experience in front-end, back-end, testing and UX/UI design, but my primary experience is front-end. Also I'm contributor for Invision Community making themes and apps.",
-    themeColor: '#e21124'
+    description: desc,
+    themeColor: '#e21124',
+    openGraph: {
+      title: CONFIG_TITLE,
+      description: desc,
+      images: [
+        {
+          url: '/icons/og.png',
+          width: 1050,
+          height: 350,
+          alt: CONFIG_TITLE
+        }
+      ]
+    }
   };
 }
 
