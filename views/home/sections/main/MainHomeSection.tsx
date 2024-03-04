788cda23c160bdafb4687d994787a29dbd2fa604
aXenDeveloper <axendeveloper@gmail.com>
2024-03-04 10:16:08 +0100

    feat: Add vitnode label in home page

--- views/home/sections/main/MainHomeSection.tsx
+++ views/home/sections/main/MainHomeSection.tsx
@@ -3,15 +3,26 @@ import cx from 'clsx';
 
 import { Button } from '@/components/button/Button';
 import style from './MainHomeSection.module.scss';
+import { Link } from '@/i18n';
 
 export const MainHomeSection = () => {
   const t = useTranslations('home');
 
   return (
     <section className={cx('layout_wrapper', style.wrapper)}>
+      <Link
+        href="https://vitnode.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className={style.vitnode}
+      >
+        {t.rich('vitnode', {
+          bold: text => <span className={style.vitnode_bold}>{text}</span>
+        })}
+      </Link>
+
       <h1 className={style.title}>{t('title')}</h1>
       <p className={style.desc}>{t('desc')}</p>
-
       <div className={style.buttons}>
         <Button id="products" href="/products">
           {t('buttons.products')}
